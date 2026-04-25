cefb08dcf3cd9399891b959f1b446489e59e7a53
Luis Palacios <palaciossanchezluis6@gmail.com>
2026-04-24 21:44:18 -0700

    se agregaron metodos get id

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const pool = require('./db');
 const app = express();
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,16 +16,31 @@ app.get('/alumnos', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener los alumnos' });
   }
 });
-app.get('/materia', async (req, res) => {
+
+app.get('/alumnos/:id', async (req, res) => {
   try {
-    const resultado = await pool.query('SELECT * FROM materia');
-    res.json(resultado.rows);
+    const { id } = req.params;
+
+    if (isNaN(id) || id.trim() === '') {
+      return res.status(400).json({ error: 'El id debe ser un número válido' });
+    }
+
+    const resultado = await pool.query(
+      'SELECT * FROM alumno WHERE id = $1',
+      [id]
+    );
+
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Alumno no encontrado' });
+    }
+
+    res.json(resultado.rows[0]);
   } catch (error) {
-    console.error('Error al consultar materias:', error);
-    res.status(500).json({ 
-      error: 'Error al obtener las materias',});
+    console.error(' Error al consultar alumno:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
+
 app.post('/alumnos', async (req, res) => {
   try {
     const { nombre, apellido, edad, correo } = req.body;
@@ -49,6 +63,42 @@ app.post('/alumnos', async (req, res) => {
     res.status(500).json({ error: 'Error al insertar el alumno' });
   }
 });
+
+app.get('/materia', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM materia');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar materias:', error);
+    res.status(500).json({ 
+      error: 'Error al obtener las materias',});
+  }
+});
+
+app.get('/materias/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(id) || id.trim() === '') {
+      return res.status(400).json({ error: 'El id debe ser un número válido' });
+    }
+
+    const resultado = await pool.query(
+      'SELECT * FROM materia WHERE id = $1',
+      [id]
+    );
+
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Materia no encontrada' });
+    }
+
+    res.json(resultado.rows[0]);
+  } catch (error) {
+    console.error('Error al consultar materia:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
 app.post('/materia', async (req, res) => {
   try {
     const { nombre, semestre, creditos } = req.body;
@@ -75,6 +125,10 @@ app.post('/materia', async (req, res) => {
       error: 'Error al insertar la materia',});
   }
 });
+
+
+
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
